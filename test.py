31b4535d3d01399310d5ce46ee63995690fb4f5d
M Dhanush <56598695+dhanush29@users.noreply.github.com>
2021-11-22 12:30:49 +0530

    added test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+
+from unittest.main import main
+from max import max
+
+class TestMax(unittest.TestCase):
+    def test_max_1(self):
+        self.assertEqual(max([4,3,1,2,5,7,6]),7)
+
+    def test_max_2(self):
+        self.assertEqual(max([1,2]),2)
+
+    def test_max_3(self):
+        self.assertEqual(max([1],1)
+
+if __name__ == '__main__':
+    unittest.main()
