f5d30652f75e9100af39368b247a5393e31e6c89
M Dhanush <56598695+dhanush29@users.noreply.github.com>
2021-11-22 22:26:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,16 +3,17 @@
 import unittest
 
 from unittest.main import main
+from mymax import mymax
 
 class TestMax(unittest.TestCase):
     def test_max_1(self):
-        self.assertEqual(max([4,3,1,2,5,7,6]),7)
+        self.assertEqual(mymax([4,3,1,2,5,7,6]),7)
 
     def test_max_2(self):
-        self.assertEqual(max([1,2]),2)
+        self.assertEqual(mymax([1,2]),2)
 
     def test_max_3(self):
-        self.assertEqual(max([1],1)
+        self.assertEqual(mymax([1],1)
 
 if __name__ == '__main__':
-        unittest.main()
+    unittest.main()
