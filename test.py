2365ed4a088b744d05a3b4a5220a56106c55fe91
M Dhanush <56598695+dhanush29@users.noreply.github.com>
2021-11-22 22:40:10 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ class TestMax(unittest.TestCase):
 
     def test_max_2(self):
         self.assertEqual(mymax([1,2]),2)
+        
+    def test_max_3(self):
+        self.assertEqual(max[1],1)
 
 if __name__ == '__main__':
     unittest.main()
